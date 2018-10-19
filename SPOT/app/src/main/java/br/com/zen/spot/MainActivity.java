065235e9ce3d8bd0ca75c14cc1fe0e4af742c532
Zenobio <joaogabrielzenobio@gmail.com>
2018-10-19 00:16:17 -0300

    Login pronto

---
+++ SPOT/app/src/main/java/br/com/zen/spot/MainActivity.java
@@ -0,0 +1,14 @@
+package br.com.zen.spot;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        getSupportActionBar().hide();
+        setContentView(R.layout.activity_main);
+    }
+}
